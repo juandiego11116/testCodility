67f18d84b07094c45b792148ec0fe4b1e3460152
juandiego11116 <71513912+juandiego11116@users.noreply.github.com>
2022-06-27 10:36:37 -0500

    style: add space in comment

--- testCodility/src/testCodility/StringEndsWith.java
+++ testCodility/src/testCodility/StringEndsWith.java
@@ -1,5 +1,41 @@
 package testCodility;
 
 public class StringEndsWith {
+	/*
+	 * DESCRIPTION:
+		Complete the solution so that it returns true if the first argument(string) 
+		passed in ends with the 2nd argument (also a string).
+		
+		Examples:
+
+		solution('abc', 'bc') // returns true
+		solution('abc', 'd') // returns false
+	 * */
+	
+	public static boolean solution(String str, String ending) {
+	    
+	    int sizeStr = str.length()-1;
+	    int sizeEnding = ending.length();
+	    String result = "";
+	    
+	    
+	    if(str.length()>=ending.length()){
+	    for (int i = 0; i<sizeEnding; i++) {
+	      result += str.charAt(sizeStr);
+	      sizeStr = sizeStr -1;
+	    }
+	    
+	    StringBuilder strb = new StringBuilder(result);
+	    result = strb.reverse().toString();
+	    
+	    if(ending.equals(result)) {
+	      return true;
+	    }else {
+	      return false;
+	    }
+	      
+	  }else {
+	      return false;
+	    }}
 
 }
