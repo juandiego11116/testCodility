8b1ff9aa137f8657bf6972f12e3c8cf0bd5ec5ed
André Rodrigues <andre.rodrigues@aramisauto.com>
2022-07-18 14:43:55 -0500

    code review

--- testCodility/src/testCodility/BitCounting.java
+++ testCodility/src/testCodility/BitCounting.java
@@ -2,15 +2,15 @@ package testCodility;
 
 public class BitCounting {
 	public static void main(String[] args) {
-		int n = 1234;
+		int n = 1234; 
 		int position = 0, count = 0;
-        String binaryString = Integer.toBinaryString(n);
+        String binaryString = Integer.toBinaryString(n);//indentation is wrong
         
-        while (position != -1) { 
-        	count++;          
+        while (position != -1) { //indentation is wrong
+        	count++;     // is there is no 1 in the number n, you will return 1, which looks like it's not the expetec result      
                                            
         	position = binaryString.indexOf(1, position + 1);
-        }
+        }//indentation is wrong
         
         
 	}
