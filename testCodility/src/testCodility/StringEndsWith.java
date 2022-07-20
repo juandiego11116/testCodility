f3165f495381ffd791b19bc0e46fe7792012ab4c
André Rodrigues <andre.rodrigues@aramisauto.com>
2022-07-20 10:08:51 -0500

    code review

--- testCodility/src/testCodility/StringEndsWith.java
+++ testCodility/src/testCodility/StringEndsWith.java
@@ -14,28 +14,28 @@ public class StringEndsWith {
 	
 	public static boolean solution(String str, String ending) {
 	    
-	    int sizeStr = str.length()-1;
+	    int sizeStr = str.length()-1; //setting the size of the string -1 is misleading
 	    int sizeEnding = ending.length();
 	    String result = "";
 	    
 	    
-	    if(str.length()>=ending.length()){
-	    for (int i = 0; i<sizeEnding; i++) {
+	    if(str.length()>=ending.length()){ // spacing is not correct: if (str.length() >= ending.length()) {
+	    for (int i = 0; i<sizeEnding; i++) { // spacing is not correct: i < sizeEnding
 	      result += str.charAt(sizeStr);
 	      sizeStr = sizeStr -1;
 	    }
 	    
 	    StringBuilder strb = new StringBuilder(result);
-	    result = strb.reverse().toString();
+	    result = strb.reverse().toString(); //if you just keep the end of the str, you can commpare directly with ending, without the need to reverse it
 	    
-	    if(ending.equals(result)) {
+	    if(ending.equals(result)) { // spacing is not correct: if (ending
 	      return true;
-	    }else {
+	    }else { // } else {
 	      return false;
 	    }
-	      
+	      //useless space that does not improve readability
 	  }else {
 	      return false;
-	    }}
-
+	    }}// indentation is wrong
+	      //useless space that does not improve readability
 }
